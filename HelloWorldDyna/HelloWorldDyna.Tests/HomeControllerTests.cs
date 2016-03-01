aec4f8e98c1133d5eb4ef970c641a63bdea50247
Rasmus Johansson <rasmus.johansson@dynabyte.se>
2016-03-01 18:35:09 +0100

    Added test project.

---
+++ HelloWorldDyna/HelloWorldDyna.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDyna.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void ThisIsASillyTest()
+        {
+            //Arrange
+            bool shouldBeTrue;
+
+            //Act
+            shouldBeTrue = true;
+
+            //Assert
+            Assert.AreEqual(true, shouldBeTrue);
+        }
+    }
+}
