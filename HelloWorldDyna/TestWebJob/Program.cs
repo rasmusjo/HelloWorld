38cf9d92d4546b7a72ab1eff7b7d5a12000e7eed
Rasmus Johansson <rasmus.johansson@dynabyte.se>
2016-03-02 10:55:38 +0100

    Added WebJob.

---
+++ HelloWorldDyna/TestWebJob/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebJob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!! " + DateTime.UtcNow);
+            }
+        }
+    }
+}
